58e61c4fa3428f9df18f9775e5900487dcd3416f
IFTE-13 <ifte.phoenix@gmail.com>
2025-01-01 04:00:20 +0600

    feat (pollusion): new api

---
+++ app/api/polution/route.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+    try {
+        const apiKey = process.env.OPENWEATHER_API_KEY;
+
+        const searchParams = req.nextUrl.searchParams;
+
+        const lat = searchParams.get("lat");
+        const lon = searchParams.get("lon");
+
+        const url = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+        const response = await axios.get(url);
+        return NextResponse.json(response.data);
+
+    } catch (error) {
+        console.log("Error fetching polution data");
+        return new Response("Error fetching polution data", { status: 500 });
+    }
+}
