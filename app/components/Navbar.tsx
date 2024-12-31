e7c324dd15f7feb18d22cae919579831f56fc3f4
IFTE-13 <ifte.phoenix@gmail.com>
2025-01-01 04:59:39 +0600

    refactor (navbar): updated source code button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,22 +4,28 @@ import { github } from '@/utils/icons';
 import TheamDropDown from './TheamDropDown';
 import SearchField from './SearchField';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 
 const Navbar = () => {
     return (
-        <div className='w-full py-4 flex items-center justify-between'>
-            <div className="left"></div>
-            <div className="search-container flex shrink-0 w-full gap-2 sm:w-fit">
-                <SearchField />
-                <div className="btn-group flex items-center gap-2">
-                    <TheamDropDown />
-                    <a href="https://github.com/IFTE-13/Frosty" target="_blank">
-                        <Button>Source Code</Button>
-                    </a>
-                </div>
-            </div>
+        <div className="w-full py-4 flex items-center justify-between">
+      <div className="left hidden md:block lg:block">made by <Link href={"https://github.com/ifte-13"} target='_blank'>Ifte-13</Link></div>
+      <div className="search-container flex shrink-0 w-full gap-2 sm:w-fit">
+        <SearchField />
+
+        <div className="btn-group flex items-center gap-2">
+          <TheamDropDown />
+
+          <Button
+            className="source-code-btn flex items-center gap-2"
+            effect={"gooeyLeft"}
+          >
+            <Link href={"https://github.com/ifte-13/frosty"} target='_blank' className='flex flex-row gap-x-2 justify-center items-center'>{github} Source Code</Link>
+          </Button>
         </div>
+      </div>
+    </div>
     )
 }
 
